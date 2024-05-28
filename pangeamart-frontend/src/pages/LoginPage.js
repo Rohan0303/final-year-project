64f16eca1392fd50014dea69d844d030ee910eea
Rohan Kumar <kumar2106rohan@gmail.com>
2024-05-28 12:37:36 +0530

    project

---
+++ pangeamart-frontend/src/pages/LoginPage.js
@@ -0,0 +1,10 @@
+import Login from "../features/auth/components/Login";
+import Footer from "../features/common/Footer";
+function LoginPage() {
+    return ( <div>
+        <Login></Login>
+        <Footer></Footer>
+    </div>  );
+}
+
+export default LoginPage;
